6720b0e7df9279746fde4899af060ae5b772b854
anandskumar <anandskumar@gmail.com>
2017-10-09 21:52:01 -0700

    URLs, Reusable templates

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -22,5 +22,6 @@ from boards import views
 urlpatterns = [
 url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^$', default_urlconf),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
+# Create your views here.
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
